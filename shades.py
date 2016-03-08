1ea03f94ec63912023f636f8179507a4dfe2e321
Aanand Prasad <aanand.prasad@gmail.com>
2016-03-08 16:36:25 +0100

    Detect eyes

--- shades.py
+++ shades.py
@@ -6,6 +6,7 @@ image_path = sys.argv[1]
 
 # Create the haar cascade
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
 # Read the image
 image = cv2.imread(image_path)
@@ -25,5 +26,11 @@ print("Found {0} faces!".format(len(faces)))
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = image[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    print("Found {0} eyes!".format(len(eyes)))
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 0, 255), 2)
 
 cv2.imwrite("out.jpg", image)
