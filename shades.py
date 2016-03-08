8d1decb86e968bead5585bc9a21bf9e34049b2a4
Aanand Prasad <aanand.prasad@gmail.com>
2016-03-08 16:32:31 +0100

    Cleanup

--- shades.py
+++ shades.py
@@ -2,23 +2,22 @@ import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+image_path = sys.argv[1]
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 # Read the image
-image = cv2.imread(imagePath)
+image = cv2.imread(image_path)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Detect faces in the image
-faces = faceCascade.detectMultiScale(
+faces = face_cascade.detectMultiScale(
     gray,
     scaleFactor=1.1,
     minNeighbors=5,
     minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
+    flags=cv2.CASCADE_SCALE_IMAGE
 )
 
 print("Found {0} faces!".format(len(faces)))
